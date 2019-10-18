bb8eacd72259270e07bb3482659b34447bfed496
Bartosz Bagiński <44866581+Odbytor@users.noreply.github.com>
2019-10-18 12:48:23 +0200

    Done

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,29 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+document.getElementById("Submit").onclick = UpLowCase;
+
+function UpLowCase()
+{
+    let textEle = (<HTMLInputElement>document.getElementById("textinput")).value;
+    let a:HTMLSelectElement = <HTMLSelectElement>document.getElementById("Select")
+    let output = document.getElementById("preview");
+    let finalText;
+    if(a.options[a.selectedIndex].value == "Upp")
+    {
+        finalText = textEle.toUpperCase();
+    }
+    else
+    {
+        finalText = textEle.toLowerCase();
+    }
+
+    output.textContent = finalText
+    var table: HTMLTableElement = <HTMLTableElement>document.getElementById("Table")
+    let row = table.insertRow(0)
+    let cell = row.insertCell(0)
+    let cell1 = row.insertCell(1)
+    cell.innerHTML = table.getElementsByTagName("tbody")[0].getElementsByTagName("tr").length.toString()
+    cell1.innerHTML = finalText;
+
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+
